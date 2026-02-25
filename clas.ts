275c07bd61daa91505a1f8d4791658c506dfaaa0
Zerro2003 <mugishaelie160@gmail.com>
2026-02-25 16:24:38 +0200

    Tries class access specifies

--- clas.ts
+++ clas.ts
@@ -1,32 +1,33 @@
-class Person {
-  name: string;
-  age: number;
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
-  }
-  getName(): void {
-    console.log(`my name is ${this.name} and I am ${this.age} years old`);
-  }
-}
-const person1 = new Person("Elie Mugisha", 22);
-person1.getName();
-
-// class Smartphone {
-//   public model: string;
-//   private internalSerial: string;
-//   protected batteryHealth: number;
-
-//   constructor(model: string, serial: string) {
-//     this.model = model;
-//     this.internalSerial = serial;
-//     this.batteryHealth = 100;
+// class Person {
+//   name: string;
+//   age: number;
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.age = age;
 //   }
-
-//   public showSerial() {
-//     console.log(`The hidden serial is: ${this.internalSerial}`);
+//   getName(): void {
+//     console.log(`my name is ${this.name} and I am ${this.age} years old`);
 //   }
 // }
-// const myPhone = new Smartphone("Pixel 8", "XYZ-123");
-// console.log(myPhone.model);
-// myPhone.showSerial();
+// const person1 = new Person("Elie Mugisha", 22);
+// person1.getName();
+
+class Smartphone {
+  public model: string;
+  private internalSerial: string;
+  protected batteryHealth: number;
+
+  constructor(model: string, serial: string) {
+    this.model = model;
+    this.internalSerial = serial;
+    this.batteryHealth = 100;
+  }
+
+  public showSerial() {
+    console.log(`The hidden serial is: ${this.internalSerial}`);
+  }
+}
+const myPhone = new Smartphone("Pixel 8", "XYZ-123");
+console.log(myPhone.model);
+console.log(myPhone.internalSerial);
+myPhone.showSerial();
