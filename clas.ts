33cce12e70b3ef27736d4c94485475d12f7257c4
Zerro2003 <mugishaelie160@gmail.com>
2026-02-26 20:21:41 +0200

    adds class for testing access and properties modifies

--- clas.ts
+++ clas.ts
@@ -12,22 +12,55 @@
 // const person1 = new Person("Elie Mugisha", 22);
 // person1.getName();
 
-class Smartphone {
-  public model: string;
-  private internalSerial: string;
-  protected batteryHealth: number;
+// class Smartphone {
+//   public model: string;
+//   private internalSerial: string;
+//   protected batteryHealth: number;
 
-  constructor(model: string, serial: string) {
-    this.model = model;
-    this.internalSerial = serial;
-    this.batteryHealth = 100;
-  }
+//   constructor(model: string, serial: string) {
+//     this.model = model;
+//     this.internalSerial = serial;
+//     this.batteryHealth = 100;
+//   }
 
-  public showSerial() {
-    console.log(`The hidden serial is: ${this.internalSerial}`);
+//   public showSerial() {
+//     console.log(`The hidden serial is: ${this.internalSerial}`);
+//   }
+// }
+// const myPhone = new Smartphone("Pixel 8", "XYZ-123");
+// console.log(myPhone.model);
+// console.log(myPhone.internalSerial);
+// myPhone.showSerial();
+
+// class Smartphone {
+//   static totalPhonesCreated: number = 0;
+
+//   constructor(public model: string) {
+//     Smartphone.totalPhonesCreated++;
+//   }
+
+//   static getManufacturer(): string {
+//     return "Global Tech Corp";
+//   }
+// }
+
+// const phone1 = new Smartphone("iPhone 15");
+// const phone2 = new Smartphone("Galaxy S24");
+
+// console.log(Smartphone.totalPhonesCreated);
+// console.log(Smartphone.getManufacturer());
+
+// console.log(phone1.totalPhonesCreated);
+// console.log("Opps there is something went wrong");
+
+export function partsSums(ls: number[]): number[] {
+  let cont = [];
+  for (let a = ls.length; a >= 0; a--) {
+    if (ls.length === a) {
+      cont.push(ls);
+      cont.pop();
+    }
   }
+  return cont;
 }
-const myPhone = new Smartphone("Pixel 8", "XYZ-123");
-console.log(myPhone.model);
-console.log(myPhone.internalSerial);
-myPhone.showSerial();
+console.log(partsSums([0, 1, 3, 6, 10]));
