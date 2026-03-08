577a8a6cd78bc34161d8866675c1e6d0fd8fc0ab
Zerro2003 <mugishaelie160@gmail.com>
2026-03-08 19:24:02 +0200

    type assertion trial complete

--- assertion.ts
+++ assertion.ts
@@ -1,2 +1,6 @@
-let id: number | string = "123";
-let numericId = (id as string).toUpperCase(); // assert it's a string
+// let id: number | string = "123";
+// let numericId = (id as string).toUpperCase(); // assert it's a string
+
+let secretData: any = "Hello from Kigali";
+let getLength = (secretData as string).length;
+console.log(getLength);
