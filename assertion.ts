831fe68aa5ad70ee0593c39f59c8ea61595f1ab0
Zerro2003 <mugishaelie160@gmail.com>
2026-03-08 18:57:53 +0200

    first assertion attempt

---
+++ assertion.ts
@@ -0,0 +1,2 @@
+let id: number | string = "123";
+let numericId = (id as string).toUpperCase(); // assert it's a string
--- clas.js
+++ clas.js
@@ -1,4 +1,3 @@
-"use strict";
 // class Person {
 //   name: string;
 //   age: number;
@@ -12,8 +11,21 @@
 // }
 // const person1 = new Person("Elie Mugisha", 22);
 // person1.getName();
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.partsSums = partsSums;
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // class Smartphone {
 //   public model: string;
 //   private internalSerial: string;
@@ -46,14 +58,79 @@ exports.partsSums = partsSums;
 // console.log(Smartphone.getManufacturer());
 // console.log(phone1.totalPhonesCreated);
 // console.log("Opps there is something went wrong");
-function partsSums(ls) {
-    var cont = [];
-    for (var a = ls.length; a >= 0; a--) {
-        if (ls.length === a) {
-            cont.push(ls);
-            cont.pop();
-        }
+// class User {
+//   constructor(public username: string) {} // Short-cut: creates and sets property at once
+//   login() {
+//     console.log(`${this.username} logged in.`);
+//   }
+// }
+// class Admin extends User {
+//   constructor(
+//     username: string,
+//     public adminLevel: number,
+//   ) {
+//     super(username); // 📞 Calls the User constructor
+//   }
+//   deleteUser(target: string) {
+//     console.log(`Admin ${this.username} is deleting ${target}`);
+//   }
+// }
+// const myAdmin = new Admin("Mugisha", 1);
+// myAdmin.login(); // ✅ Inherited from User
+// myAdmin.deleteUser("Guest123"); // ✅ Specific to Admin
+// class Vehicle {
+//   constructor(protected speed: number) {}
+//   accelerate(a: number) {
+//     this.speed += a;
+//   }
+// }
+// class Car extends Vehicle {
+//   constructor(speed: number) {
+//     super(speed);
+//   }
+//   checkSpeed() {
+//     console.log(`the current speed is ${this.speed}`);
+//   }
+// }
+// const newVeh = new Car(10);
+// newVeh.accelerate(10);
+// newVeh.checkSpeed();
+// abstract class Waste {
+//   constructor(public weight: number) {}
+//   getWeight() {
+//     return `${this.weight}kg`;
+//   }
+//   abstract recycle(): void;
+// }
+// class Plastic extends Waste {
+//   recycle() {
+//     console.log("Melting down the plastic...");
+//   }
+// }
+// class Organic extends Waste {
+//   recycle() {
+//     console.log("Composting the organic waste...");
+//   }
+// }
+// const bottle = new Plastic(2);
+// console.log(bottle.getWeight());
+// bottle.recycle();
+var Shape = /** @class */ (function () {
+    function Shape(side) {
     }
-    return cont;
-}
-console.log(partsSums([0, 1, 3, 6, 10]));
+    return Shape;
+}());
+var Square = /** @class */ (function (_super) {
+    __extends(Square, _super);
+    function Square(side) {
+        var _this = _super.call(this, side) || this;
+        _this.side = side;
+        return _this;
+    }
+    Square.prototype.getArea = function () {
+        return this.side * this.side;
+    };
+    return Square;
+}(Shape));
+var calc = new Square(2);
+console.log(calc.getArea());
