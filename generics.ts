307d2628bfdd51954528e5e86d88077d1c989ec9
Zerro2003 <mugishaelie160@gmail.com>
2026-03-08 20:13:51 +0200

    Adds work codes on using generic classes

--- generics.ts
+++ generics.ts
@@ -1,7 +1,35 @@
-interface toUp {
-  toUpperCase(): string;
+// interface toUp {
+//   toUpperCase(): string;
+// }
+// function myString<T extends toUp>(name: T) {
+//   return name.toUpperCase();
+// }
+// console.log(myString("mugisha"));
+
+// interface toUp<t> {
+//   toUpperCase(): t;
+// }
+// function myString<T extends toUp<string>>(name: T) {
+//   return name.toUpperCase();
+// }
+// console.log(myString("mugisha"));
+
+class List<anylist> {
+  constructor(public theList: anylist[] = []) {}
+  addthings(elmt: anylist): void {
+    this.theList.push(elmt);
+  }
+  getAll(): anylist[] {
+    return this.theList;
+  }
 }
-function myString<T extends toUp>(name: T) {
-  return name.toUpperCase();
-}
-console.log(myString("mugisha"));
+const veges = new List<string>();
+veges.addthings("igihaza");
+veges.addthings("imboga");
+veges.addthings("intoryi");
+const marks = new List<number>();
+marks.addthings(3);
+marks.addthings(55);
+marks.addthings(21);
+console.log(marks.getAll());
+console.log(veges.getAll());
